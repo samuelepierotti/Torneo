283d3d67d66449407dee30eb4116823ad567c446
Cagmat <cagliesi.matteo@gmail.com>
2024-11-28 09:15:44 +0100

    Secondo commit

---
+++ src/torneo/Arbitro.java
@@ -0,0 +1,14 @@
+package torneo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author B045_08
+ */
+public class Arbitro {
+    
+}
